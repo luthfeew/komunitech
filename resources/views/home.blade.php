c2bcbe0f02e8da13d5596d86a7ce5a6d9ce602f1
luthfeew <luthfeew@gmail.com>
2022-12-14 14:22:53 +0700

    templating

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,94 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    <div class="container-fluid g-0">
+        <div class="d-flex flex-row justify-content-center">
+            <div class="p-2" style="width: 664px;">
+                <div class="card">
+                    <div class="card-body p-0">
+                        <div class="d-flex flex-row">
+                            <div class="d-none d-sm-flex py-2 px-1 flex-column align-items-center bg-light"
+                                style="border-radius: 0.375rem;">
+                                <div>
+                                    <button type="button" class="btn btn-outline-primary"
+                                        style="--bs-btn-padding-y: .1rem; --bs-btn-padding-x: .25rem; --bs-btn-font-size: .75rem;">
+                                        <i class="bi bi-arrow-up"></i>
+                                    </button>
+                                </div>
+                                <div class="my-1 fw-semibold">6969</div>
+                                <div>
+                                    <button type="button" class="btn btn-outline-danger"
+                                        style="--bs-btn-padding-y: .1rem; --bs-btn-padding-x: .25rem; --bs-btn-font-size: .75rem;">
+                                        <i class="bi bi-arrow-down"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="pt-2 d-flex flex-column">
+                                <div class="mx-2 mb-2 d-flex flex-row align-items-center">
+                                    <div>
+                                        <img src="https://picsum.photos/40" class="img-fluid rounded-circle me-1"
+                                            style="max-width: 20px;">
+                                    </div>
+                                    <div class="d-flex flex-row align-items-center lh-1">
+                                        <div>k/indonesia</div>
+                                        <div class="mx-1">&#x2022;</div>
+                                        <div>
+                                            <span>Posted by</span>
+                                            <span>u/entasd</span>
+                                            <span>10 jam yang lalu</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mx-2 mb-2 lh-sm fs-5 fw-semibold text-break">
+                                    If every Archon had to fight all the vision wielders of their respective
+                                    elements, how would they do?
+                                </div>
+                                <div class="ps-1 pe-2 d-flex flex-row">
+                                    <div class="mb-1 mx-1 d-sm-none d-flex flex-row align-items-center">
+                                        <div>
+                                            <button type="button" class="btn btn-outline-primary"
+                                                style="--bs-btn-padding-y: .1rem; --bs-btn-padding-x: .25rem; --bs-btn-font-size: .75rem;">
+                                                <i class="bi bi-arrow-up"></i>
+                                            </button>
+                                        </div>
+                                        <div class="mx-2 fw-semibold">6969</div>
+                                        <div>
+                                            <button type="button" class="btn btn-outline-danger"
+                                                style="--bs-btn-padding-y: .1rem; --bs-btn-padding-x: .25rem; --bs-btn-font-size: .75rem;">
+                                                <i class="bi bi-arrow-down"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <button type="button" class="btn btn-light"
+                                            style="--bs-btn-padding-x: 0.5rem; --bs-btn-padding-y: 0.2rem; --bs-btn-bg: #FFFFFF; --bs-btn-border-color: #FFFFFF; --bs-btn-font-size: .90rem;">
+                                            <i class="fa-regular fa-message"></i>
+                                            495 Comments
+                                        </button>
+                                    </div>
+                                    <div>
+                                        <button type="button" class="btn btn-light"
+                                            style="--bs-btn-padding-x: 0.5rem; --bs-btn-padding-y: 0.2rem; --bs-btn-bg: #FFFFFF; --bs-btn-border-color: #FFFFFF; --bs-btn-font-size: .90rem;">
+                                            <i class="fa-solid fa-share"></i>
+                                            Share
+                                        </button>
+                                    </div>
+                                    <div>
+                                        <button type="button" class="btn btn-light"
+                                            style="--bs-btn-padding-x: 0.5rem; --bs-btn-padding-y: 0.2rem; --bs-btn-bg: #FFFFFF; --bs-btn-border-color: #FFFFFF; --bs-btn-font-size: .90rem;">
+                                            <i class="fa-regular fa-bookmark"></i>
+                                            Save
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
                 </div>
+            </div>
+            <div class="p-2 d-none d-lg-block" style="width: 336px;">
+                asd
             </div>
         </div>
     </div>
-</div>
 @endsection
