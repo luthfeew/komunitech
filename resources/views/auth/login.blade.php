89c04a1b8210d6bf658920c97bbec9bc34f12e2c
luthfeew <luthfeew@gmail.com>
2023-01-17 15:17:52 +0700

    login & register view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,124 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Masuk - Komunitech</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="{{asset('/')}}img/favicon.png" rel="icon">
+  <link href="{{asset('/')}}img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="{{asset('/')}}vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="{{asset('/')}}vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="{{asset('/')}}vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="{{asset('/')}}vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="{{asset('/')}}vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="{{asset('/')}}vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="{{asset('/')}}vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="{{asset('/')}}css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <main>
+    <div class="container">
+
+      <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+              <div class="d-flex justify-content-center py-4">
+                <a href="index.html" class="logo d-flex align-items-center w-auto">
+                  <img src="{{asset('/')}}img/logo.png" alt="">
+                  <span class="d-none d-lg-block">Komunitech</span>
+                </a>
+              </div><!-- End Logo -->
+
+              <div class="card mb-3">
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
 
-                        <div class="row mb-3">
-                            <label for="username" class="col-md-4 col-form-label text-md-end">{{ __('Username') }}</label>
+                  <div class="pt-4 pb-2">
+                    <h5 class="card-title text-center pb-0 fs-4">Masuk ke Akun Anda</h5>
+                    <p class="text-center small">Masukkan nama pengguna & kata sandi Anda untuk masuk</p>
+                  </div>
 
-                            <div class="col-md-6">
-                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
+                  <form class="row g-3" method="POST" action="{{ route('login') }}">
+                    @csrf
 
-                                @error('username')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+                    <div class="col-12">
+                      <label for="yourUsername" class="form-label">Nama pengguna</label>
+                      <div class="input-group has-validation">
+                        <span class="input-group-text" id="inputGroupPrepend">u/</span>
+                        <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
+                        @error('username')
+                        <div class="invalid-feedback">Nama pengguna atau password salah.</div>
+                        @enderror
+                      </div>
+                    </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                    <div class="col-12">
+                      <label for="yourPassword" class="form-label">Kata sandi</label>
+                      <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                      @error('password')
+                      <div class="invalid-feedback">Silakan masukkan kata sandi Anda!</div>
+                      @enderror
+                    </div>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                    <div class="col-12">
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                        <label class="form-check-label" for="rememberMe">Ingat saya</label>
+                      </div>
+                    </div>
+                    <div class="col-12">
+                      <button class="btn btn-primary w-100" type="submit">Masuk</button>
+                    </div>
+                    <div class="col-12">
+                      <p class="small mb-0">Tidak punya akun? <a href="{{ route('register') }}">Buat akun</a></p>
+                    </div>
+                  </form>
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
                 </div>
+              </div>
+
             </div>
+          </div>
         </div>
+
+      </section>
+
     </div>
-</div>
-@endsection
+  </main><!-- End #main -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="{{asset('/')}}vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="{{asset('/')}}vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="{{asset('/')}}vendor/chart.js/chart.umd.js"></script>
+  <script src="{{asset('/')}}vendor/echarts/echarts.min.js"></script>
+  <script src="{{asset('/')}}vendor/quill/quill.min.js"></script>
+  <script src="{{asset('/')}}vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="{{asset('/')}}vendor/tinymce/tinymce.min.js"></script>
+  <script src="{{asset('/')}}vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="{{asset('/')}}js/main.js"></script>
+
+</body>
+
+</html>
